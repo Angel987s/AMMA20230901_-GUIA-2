522d32dce9a0b6a3a47b2ccbc9a040c6be02a500
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-09-02 19:54:41 -0600

    Modelo Creado Alumno

---
+++ AMMA20230901/Models/Alumno.cs
@@ -0,0 +1,10 @@
+﻿namespace AMMA20230901.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Matricula { get; set; }
+    }
+}
